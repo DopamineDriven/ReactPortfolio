400782a0831f65c51808b391b8006b18027e78bd
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-03-18 23:42:55 -0500

    render test via jest successful

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import './App.css';
+import './bootstrap.min.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div>React Portfolio</div>
+        )
+    }
+}
+
+export default App;
